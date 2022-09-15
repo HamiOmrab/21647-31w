fda1b9284f186f40db2b2a0ae8712a9aa65d71bb
Omranabasi <you@example.com>
2022-09-15 12:03:27 -0400

    ajout de sass

--- index.php
+++ index.php
@@ -14,24 +14,10 @@
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon WP</title>
-    <style>
-        body {
-            background-color: aquagit;
-        }
 
-        h1 {
-            
-        }
-    </style>
-</head>
-<body>
+
+<?php get_header(); ?>
+
     <main>
         <?php
             if ( have_posts() ) :
@@ -43,5 +29,6 @@
             endif;
         ?>
     </main>
-</body>
+
+    <?php get_footer(); ?>  
 </html>
