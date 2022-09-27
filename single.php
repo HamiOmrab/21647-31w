d234d15fb617a8b3fe87c5c6ad40d60047ae9150
Omranabasi <you@example.com>
2022-09-27 16:02:51 -0400

    ajoute de front-page.php et single.php

---
+++ single.php
@@ -0,0 +1,53 @@
+
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+
+<h1 class="trace">single page</h1>
+<?php get_header(); ?>
+
+<main>
+    <?php
+		if ( have_posts() ) :
+            while ( have_posts() ) :
+				the_post(); 
+                the_title('<h1>','</h1>');
+                the_content(null, true);
+    ?>
+                <section>
+                    <small>
+                    <span> <?php the_weekday(); ?> </span>
+                    <span> <?php the_date(); ?>    </span>
+                    <span> <?php the_time(); ?>    </span>
+                    </small>       
+                </section>
+                <code>
+                    <span> <?php the_author(); ?> </span>
+                </code>
+                <pre>
+                    <span> <?php the_category(); ?> </span>
+                </pre>
+    <?php
+            endwhile;
+        endif;
+    ?>    
+    </main>    
+<?php get_footer(); ?>
+</html>
+
+
+
+
+
+
