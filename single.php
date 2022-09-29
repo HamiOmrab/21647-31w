03a5c1ec500095bcd1fc6f1d3a29d42bc0147edd
Omranabasi <you@example.com>
2022-09-29 10:38:41 -0400

    changement single.php

--- single.php
+++ single.php
@@ -21,7 +21,7 @@
     <?php
 		if ( have_posts() ) :
             while ( have_posts() ) :
-				the_post(); 
+				 
                 the_title('<h1>','</h1>');
                 the_content(null, true);
     ?>
