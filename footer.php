0fc03171cc893f3301bade939d5335d4149c837a
Omranabasi <you@example.com>
2022-10-25 16:38:30 -0400

    C13 - ajoute deuxiem widget

--- footer.php
+++ footer.php
@@ -12,10 +12,10 @@
 ?>
 
 	<footer class="site__footer">
-		<div> <?php get_sidebar( 'primary' ); ?> </div>
-		<div>Lorem, ipsum dolor.</div>
-		<div>Lorem, ipsum dolor.</div>
-		<div>Lorem, ipsum dolor.</div>
+		<div><?php get_sidebar( 'footer-1' ); ?></div>
+		<div><?php get_sidebar( 'footer-2' ); ?></div>
+		<div>lorem3</div>
+		<div>lorem3</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
