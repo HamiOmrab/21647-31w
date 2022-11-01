61fb961a2ef78932f9b8ae8faee68926d388ca58
Omranabasi <e2295313@cmaisonneuve.qc.ca>
2022-11-01 16:48:10 -0400

    C15 - créer category-cours.php et changement front-page.php

--- front-page.php
+++ front-page.php
@@ -1,3 +1,31 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@HamiOmrab 
+eddytuto
+/
+21647-31w
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+21647-31w/front-page.php /
+@eddypmartin
+eddypmartin c15-Début du de l'exercice #3. Liste des cours avec category-cours.ph…
+…
+Latest commit 76281af 7 minutes ago
+ History
+ 1 contributor
+40 lines (29 sloc)  926 Bytes
+
 
 <?php
 /**
@@ -13,33 +41,23 @@
  * @package underscore
  */
 ?>
-
-
 <!-- h1 class="trace">front-page.php</h1 -->
 <?php get_header(); ?>
-<!-- h1 class="trace">front-page.php</h1 -->
 
     <main class="site__main">
     <?php
 		if ( have_posts() ) :
             while ( have_posts() ) :
-				the_post();
-    ?>
-    <h1><a href="<?php the_permalink(); ?>">
-    <?php the_title(); ?> </a></h1>
-    <h2> Durée du cours: <?= the_field('duree')."h"; ?></h2>
+				the_post(); ?>
+                <h1><a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?></a></h1>
+                
 
-    <?php the_content(null, true); ?>
-    
-    <?php endwhile;
-        endif;
-    ?>    
+                <?php the_content(null, true); ?>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+  
     </main>    
 <?php get_footer(); ?>
 </html>
-
-
-
-
-
-
