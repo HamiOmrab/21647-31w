7bd8c25e37a4b01df9bd58126327c9e1c88b8d6f
Omranabasi <e2295313@cmaisonneuve.qc.ca>
2022-11-01 15:33:19 -0400

    C15 - Début de l'exercice #3.

--- front-page.php
+++ front-page.php
@@ -27,7 +27,7 @@
     ?>
     <h1><a href="<?php the_permalink(); ?>">
     <?php the_title(); ?> </a></h1>
-    <h2> Durée du cours <?php the_field('durée'); ?></h2>
+    <h2> Durée du cours: <?= the_field('duree')."h"; ?></h2>
 
     <?php the_content(null, true); ?>
     
