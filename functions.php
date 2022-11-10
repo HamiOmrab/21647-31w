0e7b5f5ed31d7c780980dc42d56273378cf74efa
Omranabasi <e2295313@cmaisonneuve.qc.ca>
2022-11-10 11:24:13 -0500

    C17 - Ajoute  «prefix_nav_description» à function.php

--- functions.php
+++ functions.php
@@ -103,6 +103,26 @@ function igc31w_filtre_choix_menu($obj_menu, $arg){
 
 add_filter("wp_nav_menu_objects","igc31w_filtre_choix_menu", 10,2);
 
+/**
+ * filtre le menu «evenement»
+ * @arg  string   $item_output	string représantant l'élément du menu
+ * @arg  obj      $item			element du menu
+ */
+/* ----------------------------------------------------------- Ajout de la description dans menu */
+function prefix_nav_description( $item_output, $item) {
+    if ( !empty( $item->description ) ) {
+        $item_output = str_replace( '</a>',
+        '<hr><span class="menu-item-description">' . $item->description . '</span><div class="menu-item-icone"></div></a>',
+              $item_output );
+    }
+    return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 2 );
+// l'argument 10 : niveau de privilège
+// l'argument 2 : le nombre d'argument dans la fonction de rappel: «prefix_nav_description»
+
+
+
 /**----------------------------------------------------------------------Initialisation des sidebar */
 add_action( 'widgets_init', 'my_register_sidebars' );
 function my_register_sidebars() {
