28e3f95735a58112bde73f0e92e80a9888cabb05
Omranabasi <e2295313@cmaisonneuve.qc.ca>
2022-11-18 13:08:36 -0500

    C18 - coriger fonction.php et header.php

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '1.0.0' );
 }
 
+require_once("options/apparence.php");
 //////////////////////////////////////////////////
 function underscore_setup() {
 
--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<style>.site__header { background-color:<?= get_theme_mod("site__title__background"); ?>;}</style>
 	<?php wp_head(); ?>
 </head>
 
